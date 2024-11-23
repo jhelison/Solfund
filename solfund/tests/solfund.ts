234ae2eb37d58d9a988c2def9848f8822237240d
Jhelison Uchoa <jhelison@brickabode.com>
2024-11-22 23:25:55 -0300

    test(program): Add remove contribution tests

--- solfund/tests/solfund.ts
+++ solfund/tests/solfund.ts
@@ -22,7 +22,7 @@ describe("solfund", () => {
   const default_goal = 1_000_000_000; // 1 sol
   const default_URI = "ipfs://example_metadata_uri";
 
-  describe("Initialize the program", async () => {
+  describe("New campaign", async () => {
     it("Initialize correctly", async () => {
       // Airdrop tokens for the deployment
       const deployer = await fundNewKey(provider.connection);
@@ -39,19 +39,17 @@ describe("solfund", () => {
         );
 
       // Check the final result
-      await checkCampaign(
-        program,
-        campaign_pda,
-        campaign_bump,
-        deployer.publicKey,
-        default_goal,
-        default_title,
-        default_end_ts,
-        default_URI,
-        0,
-        false,
-        false
-      );
+      await checkCampaign(program, campaign_pda, {
+        bump: campaign_bump,
+        end_ts: default_end_ts,
+        goal: default_goal,
+        is_successful: false,
+        is_withdrawn: false,
+        metadata_uri: default_URI,
+        owner: deployer.publicKey,
+        title: default_title,
+        total_funds: 0,
+      });
     });
     it("Fail with timestamp under the 1 day constraint", async () => {
       // Airdrop tokens for the deployment
@@ -161,17 +159,308 @@ describe("solfund", () => {
             default_end_ts,
             default_URI
           );
+      } catch (error) {
+        assert.strictEqual(
+          error.toString(),
+          "TypeError: Max seed length exceeded"
+        );
+      }
+    });
+  });
+  describe("New contribution", async () => {
+    let default_campaign_pda: anchor.web3.PublicKey;
+    let total_contributed: number;
 
-        await checkCampaign(
+    before(async () => {
+      // Airdrop tokens for the deployment
+      let deployer = await fundNewKey(provider.connection);
+
+      // Initialize the campaign
+      let _bump: any;
+      [default_campaign_pda, _bump] = await createAndInitializeCampaignAccount(
+        program,
+        deployer,
+        default_title,
+        default_goal,
+        default_end_ts,
+        default_URI
+      );
+    });
+
+    it("Contribute correctly", async () => {
+      // Create the contributor
+      const contributor = await fundNewKey(provider.connection);
+
+      // Get the balance for the testing accounts
+      const campaign_balance = await provider.connection.getBalance(
+        default_campaign_pda
+      );
+      const contributor_balance = await provider.connection.getBalance(
+        contributor.publicKey
+      );
+
+      // Try to do a contribution
+      const contribution_amount = 100000000;
+      const contribution_pda = await createAndInitializeContribution(
+        program,
+        contributor,
+        default_campaign_pda,
+        contribution_amount
+      );
+
+      // Check the account
+      await checkContribution(program, contribution_pda, {
+        contributor: contributor.publicKey,
+        amount: contribution_amount,
+      });
+
+      // Balances should have been updated
+      const campaign_balance_new = await provider.connection.getBalance(
+        default_campaign_pda
+      );
+      const contributor_balance_new = await provider.connection.getBalance(
+        contributor.publicKey
+      );
+
+      // The campaign now should have a new balance
+      assert.strictEqual(
+        campaign_balance_new,
+        campaign_balance + contribution_amount
+      );
+      // The contributor should have lost balance
+      assert.isTrue(contributor_balance_new < contributor_balance);
+
+      // Check the campaign total
+      total_contributed += contribution_amount;
+      await checkCampaign(program, default_campaign_pda, {
+        total_funds: total_contributed,
+      });
+    });
+    it("Do multiple contributions", async () => {
+      // Check the campaign balance
+      const campaign_balance = await provider.connection.getBalance(
+        default_campaign_pda
+      );
+
+      // Track the total
+      let total_curr_contributed = 0;
+      for (let i = 1; i < 4; i++) {
+        // Create the contributor
+        const contributor = await fundNewKey(provider.connection);
+
+        // Do contributions based on the index
+        const contribution_amount = 10000000 * i;
+        await createAndInitializeContribution(
           program,
-          campaign_pda,
-          campaign_bump,
-          null,
-          null,
-          "asd"
+          contributor,
+          default_campaign_pda,
+          contribution_amount
+        );
+        total_curr_contributed += contribution_amount;
+      }
+
+      // Check the campaign balance
+      const campaign_balance_new = await provider.connection.getBalance(
+        default_campaign_pda
+      );
+
+      // The campaign now should have a new balance
+      assert.strictEqual(
+        campaign_balance_new,
+        campaign_balance + total_curr_contributed
+      );
+
+      // Check the campaign total
+      total_contributed += total_curr_contributed;
+      await checkCampaign(program, default_campaign_pda, {
+        total_funds: total_contributed,
+      });
+    });
+    it("Should not contribute with zero balance", async () => {
+      // Create the contributor with low balance
+      const contributor = await fundNewKey(provider.connection, 10000000);
+
+      // Try to do a contribution
+      const contribution_amount = 100000000;
+      try {
+        await createAndInitializeContribution(
+          program,
+          contributor,
+          default_campaign_pda,
+          contribution_amount
+        );
+        // The code must fail
+        assert.isTrue(false);
+      } catch (error) {
+        // The code must reach here
+        assert.isTrue(true);
+      }
+    });
+    it("Should not contribute zero", async () => {
+      // Create the contributor
+      const contributor = await fundNewKey(provider.connection);
+
+      // Try to do a contribution with zero balance
+      try {
+        const contribution_amount = 0;
+        await createAndInitializeContribution(
+          program,
+          contributor,
+          default_campaign_pda,
+          contribution_amount
         );
       } catch (error) {
-        assert.strictEqual(error.toString(), "TypeError: Max seed length exceeded");
+        assert.strictEqual(error.error.errorCode.code, "ZeroContribution");
+      }
+    });
+    it("Should not contribute with campaign expired", async () => {
+      // We create a new campaign deployment, with one second into the future
+      const curr_timestamp = Math.floor(Date.now() / 1000);
+      let [new_campaign_pda, _bump] = await createAndInitializeCampaignAccount(
+        program,
+        await fundNewKey(provider.connection),
+        default_title,
+        default_goal,
+        curr_timestamp + 1, // One second into the future
+        default_URI
+      );
+
+      // Sleep for one second and a half
+      await sleep(1500);
+
+      // Now try to contribute
+      try {
+        // Create the contributor
+        const contributor = await fundNewKey(provider.connection);
+
+        // Try to contribute
+        const contribution_amount = 1000000;
+        await createAndInitializeContribution(
+          program,
+          contributor,
+          new_campaign_pda,
+          contribution_amount
+        );
+      } catch (error) {
+        assert.strictEqual(
+          error.error.errorCode.code,
+          "InteractionWithClosedCampaign"
+        );
+      }
+    });
+  });
+  describe("Remove contribution", async () => {
+    let default_campaign_pda: anchor.web3.PublicKey;
+
+    before(async () => {
+      // Airdrop tokens for the deployment
+      let deployer = await fundNewKey(provider.connection);
+
+      // Initialize the campaign
+      let _bump: any;
+      [default_campaign_pda, _bump] = await createAndInitializeCampaignAccount(
+        program,
+        deployer,
+        default_title,
+        default_goal,
+        default_end_ts,
+        default_URI
+      );
+    });
+    it("Withdraw correctly", async () => {
+      // Create two contributors
+      const contributor1 = await fundNewKey(provider.connection);
+      const contributor2 = await fundNewKey(provider.connection);
+
+      // Do two contributions
+      const contribution_amount1 = 100000000;
+      const contribution_pda1 = await createAndInitializeContribution(
+        program,
+        contributor1,
+        default_campaign_pda,
+        contribution_amount1
+      );
+      const contribution_amount2 = 150000000;
+      await createAndInitializeContribution(
+        program,
+        contributor2,
+        default_campaign_pda,
+        contribution_amount2
+      );
+
+      // Now we do a single withdraw
+      await program.methods
+        .removeContribution()
+        .accounts({
+          campaign: default_campaign_pda,
+          contribution: contribution_pda1,
+          contributor: contributor1.publicKey,
+          systemProgram: anchor.web3.SystemProgram.programId,
+        } as any)
+        .signers([contributor1])
+        .rpc();
+
+      // Check the balance of the campaign
+      const campaign_balance = await provider.connection.getBalance(
+        default_campaign_pda
+      );
+
+      // The campaign now should have the balance from account two
+      assert.strictEqual(
+        campaign_balance,
+        contribution_amount2 + 2115840 // Extra value for the account rent
+      );
+
+      // The account now must hold only share from user 2
+      await checkCampaign(program, default_campaign_pda, {
+        total_funds: contribution_amount2,
+      });
+    });
+    it("Can't withdraw on a closed campaign", async () => {
+      // We create a new campaign deployment, with one second into the future
+      const curr_timestamp = Math.floor(Date.now() / 1000);
+      let [new_campaign_pda, _bump] = await createAndInitializeCampaignAccount(
+        program,
+        await fundNewKey(provider.connection),
+        default_title,
+        default_goal,
+        curr_timestamp + 1, // One second into the future
+        default_URI
+      );
+
+      // Create contributor
+      const contributor = await fundNewKey(provider.connection);
+
+      // Do a contribution
+      const contribution_amount = 100000000;
+      const contribution_pda = await createAndInitializeContribution(
+        program,
+        contributor,
+        new_campaign_pda,
+        contribution_amount
+      );
+
+      // Sleep
+      await sleep(1500);
+
+      // Now we try to withdraw
+      try {
+        // Now we do a single withdraw
+        await program.methods
+          .removeContribution()
+          .accounts({
+            campaign: new_campaign_pda,
+            contribution: contribution_pda,
+            contributor: contributor.publicKey,
+            systemProgram: anchor.web3.SystemProgram.programId,
+          } as any)
+          .signers([contributor])
+          .rpc();
+      } catch (error) {
+        assert.strictEqual(
+          error.error.errorCode.code,
+          "InteractionWithClosedCampaign"
+        );
       }
     });
   });
@@ -200,11 +489,40 @@ async function createAndInitializeCampaignAccount(
       campaign: campaign_pda,
       owner: owner.publicKey,
       systemProgram: anchor.web3.SystemProgram.programId,
-    })
+    } as any) // Cast as any because TS is crazy
     .signers([owner])
     .rpc();
 
   return [campaign_pda, campaign_bump];
+}
+
+// Create a new contribution
+async function createAndInitializeContribution(
+  program: anchor.Program<Solfund>,
+  contributor: anchor.web3.Keypair,
+  campaign: anchor.web3.PublicKey,
+  amount: number
+): Promise<anchor.web3.PublicKey> {
+  // Generate the PDA for the account
+  const [contribution_pda, _] = getContributionPDA(
+    contributor.publicKey,
+    campaign,
+    program.programId
+  );
+
+  // Do the contribution
+  await program.methods
+    .newContribution(new anchor.BN(amount))
+    .accounts({
+      campaign: campaign,
+      contribution: contribution_pda,
+      contributor: contributor.publicKey,
+      systemProgram: anchor.web3.SystemProgram.programId,
+    } as any) // Cast as any because TS is crazy
+    .signers([contributor])
+    .rpc(); // Need to skip pre-flight for timing errors
+
+  return contribution_pda;
 }
 
 // Get the PDA for a campaign
@@ -217,7 +535,23 @@ function getCampaignPDA(
     [
       anchor.utils.bytes.utf8.encode("Campaign"),
       owner.toBuffer(),
-      anchor.utils.bytes.utf8.encode(title)
+      anchor.utils.bytes.utf8.encode(title),
+    ],
+    program_id
+  );
+}
+
+// Get the PDA for a contribution
+function getContributionPDA(
+  contributor: anchor.web3.PublicKey,
+  campaign: anchor.web3.PublicKey,
+  program_id: anchor.web3.PublicKey
+) {
+  return anchor.web3.PublicKey.findProgramAddressSync(
+    [
+      anchor.utils.bytes.utf8.encode("Contribution"),
+      contributor.toBuffer(),
+      campaign.toBuffer(),
     ],
     program_id
   );
@@ -227,62 +561,99 @@ function getCampaignPDA(
 async function checkCampaign(
   program: anchor.Program<Solfund>,
   campaign: anchor.web3.PublicKey,
-  bump?: number,
-  owner?: anchor.web3.PublicKey,
-  goal?: number,
-  title?: string,
-  end_ts?: number,
-  metadata_uri?: string,
-  total_funds?: number,
-  is_successful?: boolean,
-  is_withdrawn?: boolean
+  expected: {
+    bump?: number;
+    owner?: anchor.web3.PublicKey;
+    goal?: number;
+    title?: string;
+    end_ts?: number;
+    metadata_uri?: string;
+    total_funds?: number;
+    is_successful?: boolean;
+    is_withdrawn?: boolean;
+  }
 ) {
   // Fetch the data
   let campaign_data = await program.account.campaign.fetch(campaign);
 
   // Start the checks
-  if (bump) {
-    assert.strictEqual(campaign_data.bump.toString(), bump.toString());
+  if (expected.bump) {
+    assert.strictEqual(campaign_data.bump.toString(), expected.bump.toString());
   }
-  if (owner) {
-    assert.strictEqual(campaign_data.owner.toString(), owner.toString());
+  if (expected.owner) {
+    assert.strictEqual(
+      campaign_data.owner.toString(),
+      expected.owner.toString()
+    );
   }
-  if (goal) {
-    assert.strictEqual(campaign_data.goal.toString(), goal.toString());
+  if (expected.goal) {
+    assert.strictEqual(campaign_data.goal.toString(), expected.goal.toString());
   }
-  if (title) {
+  if (expected.title) {
     const title_from_data = convertByteToString(
       campaign_data.title,
       campaign_data.titleLength
     );
-    assert.strictEqual(title_from_data, title);
+    assert.strictEqual(title_from_data, expected.title);
   }
-  if (end_ts) {
-    assert.strictEqual(campaign_data.endTs.toString(), end_ts.toString());
+  if (expected.end_ts) {
+    assert.strictEqual(
+      campaign_data.endTs.toString(),
+      expected.end_ts.toString()
+    );
   }
-  if (metadata_uri) {
+  if (expected.metadata_uri) {
     const uri_from_data = convertByteToString(
       campaign_data.metadataUri,
       campaign_data.metadataUriLength
     );
-    assert.strictEqual(uri_from_data, metadata_uri);
+    assert.strictEqual(uri_from_data, expected.metadata_uri);
   }
-  if (total_funds) {
+  if (expected.total_funds) {
     assert.strictEqual(
       campaign_data.totalFunds.toString(),
-      total_funds.toString()
+      expected.total_funds.toString()
     );
   }
-  if (is_successful) {
+  if (expected.is_successful) {
     assert.strictEqual(
       campaign_data.isSuccessful.toString(),
-      is_successful.toString()
+      expected.is_successful.toString()
     );
   }
-  if (is_withdrawn) {
+  if (expected.is_withdrawn) {
     assert.strictEqual(
       campaign_data.isWithdrawn.toString(),
-      is_withdrawn.toString()
+      expected.is_withdrawn.toString()
+    );
+  }
+}
+
+// Check a contribution
+async function checkContribution(
+  program: anchor.Program<Solfund>,
+  contribution: anchor.web3.PublicKey,
+  expected: {
+    contributor?: anchor.web3.PublicKey;
+    amount?: number;
+  }
+) {
+  // Fetch the data
+  let contribution_data = await program.account.contribution.fetch(
+    contribution
+  );
+
+  // Check the data
+  if (expected.contributor) {
+    assert.strictEqual(
+      contribution_data.contributor.toString(),
+      expected.contributor.toString()
+    );
+  }
+  if (expected.amount) {
+    assert.strictEqual(
+      contribution_data.amount.toString(),
+      expected.amount.toString()
     );
   }
 }
@@ -336,4 +707,9 @@ function convertByteToString(data: number[], size: number) {
 
   // Return the string
   return decoder.decode(uint8Array);
+}
+
+// Make the system sleep, good for timing tests
+function sleep(ms: number): Promise<void> {
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
